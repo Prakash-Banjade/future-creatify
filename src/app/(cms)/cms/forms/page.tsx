844e5b9837ee467e72e81670e72610be6a811d88
sameer <basnetsameer78@gmail.com>
2025-10-09 23:09:25 +0545

    forced cms forms to  ssr

--- src/app/(cms)/cms/forms/page.tsx
+++ src/app/(cms)/cms/forms/page.tsx
@@ -1,34 +1,33 @@
-import ContainerLayout from "@/components/cms/container-layout"
-import FormsList from "@/components/cms/forms/forms-list"
-import { Button } from "@/components/ui/button"
-import { Plus } from "lucide-react"
-import Link from "next/link"
-import { Suspense } from "react"
-import { TDataSearchParams } from "../../../../../types/global.types"
+import ContainerLayout from "@/components/cms/container-layout";
+import FormsList from "@/components/cms/forms/forms-list";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import { Suspense } from "react";
+import { TDataSearchParams } from "../../../../../types/global.types";
+
+export const dynamic = "force-dynamic";
 
 export type FormsPageProps = {
-    searchParams: Promise<TDataSearchParams>
-}
+  searchParams: Promise<TDataSearchParams>;
+};
 
 export default function FormsPage(props: FormsPageProps) {
-    return (
-        <ContainerLayout
-            title='Forms'
-            actionTrigger={
-                <Button
-                    type="button"
-                    asChild
-                >
-                    <Link href={"forms/new"}>
-                        <Plus />
-                        Create
-                    </Link>
-                </Button>
-            }
-        >
-            <Suspense fallback={<div>Loading...</div>}>
-                <FormsList {...props} />
-            </Suspense>
-        </ContainerLayout>
-    )
+  return (
+    <ContainerLayout
+      title="Forms"
+      actionTrigger={
+        <Button type="button" asChild>
+          <Link href={"forms/new"}>
+            <Plus />
+            Create
+          </Link>
+        </Button>
+      }
+    >
+      <Suspense fallback={<div>Loading...</div>}>
+        <FormsList {...props} />
+      </Suspense>
+    </ContainerLayout>
+  );
 }
